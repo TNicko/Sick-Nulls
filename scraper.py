047591ec2de8dbd2024d7a8d42df4157d078a823
Andre Pinho <andre.s.pinho@ua.pt>
2023-10-24 23:12:45 +0200

    Basic scrapper for 5G routers

---
+++ scraper.py
@@ -0,0 +1,97 @@
+import requests
+import json
+from decouple import config
+
+password = config("ERIKSON_PASSWORD")
+username = config("ERIKSON_USER")
+
+
+def get_data(ip):
+    # Get cookie and token
+    cookies = {
+        "CGISID": "NULL",
+    }
+
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101"
+            " Firefox/110.0"
+        ),
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "http://" + ip + "/login.html",
+        "Content-Type": "json",
+        "X-Requested-With": "XMLHttpRequest",
+        "Origin": "http://" + ip,
+        "Connection": "keep-alive",
+    }
+
+    params = {
+        "_": "set_web_user_login_0.010597065160396713",
+    }
+
+    data = (
+        '{"action":"set_web_user_login","args":{"user":"'
+        + username
+        + '","password":"'
+        + password
+        + '"}}'
+    )
+
+    response = requests.post(
+        "http://" + ip + "/cgi-bin/login.cgi",
+        params=params,
+        cookies=cookies,
+        headers=headers,
+        data=data,
+    )
+
+    aaa = response.headers["Set-Cookie"]
+
+    cookies = {"CGISID": aaa.split(";")[0].split("=")[1]}
+
+    # Get data
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101"
+            " Firefox/110.0"
+        ),
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "http://" + ip + "/index.html",
+        "Content-Type": "json",
+        "X-Requested-With": "XMLHttpRequest",
+        "Origin": "http://" + ip,
+        "Connection": "keep-alive",
+    }
+
+    params = {
+        "_": "get_wwan_radio_info3_0.8110112846379409",
+    }
+
+    data = (
+        '{"action":"get_wwan_radio_info3","token":"'
+        + response.json()["set_web_user_login"]["cgitoken"]
+        + '"}'
+    )
+
+    while True:
+        response = requests.post(
+            "http://" + ip + "/cgi-bin/gui.cgi",
+            params=params,
+            cookies=cookies,
+            headers=headers,
+            data=data,
+        )
+
+        resp = response.json()["get_wwan_radio_info3"]
+        yield (
+            resp["nr_srv_cell_rsrp_0"],
+            resp["nr_srv_cell_rsrp_1"],
+            resp["nr_srv_cell_rsrp_2"],
+            resp["nr_srv_cell_rsrp_3"],
+        )
+
+
+for i in get_data("192.168.1.1"):
+    print(i)

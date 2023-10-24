fff0530b989b460782b036ad6919985b9c44d81b
Nickolai Tchesnokov <nicko.tchesnokov@gmail.com>
2023-10-25 00:27:56 +0200

    outputting snr, rsrp, rsrq. (gitignore added)

--- scraper.py
+++ scraper.py
@@ -1,61 +1,91 @@
 import requests
 import json
+import time
+
 
 def get_data(ip):
+    # Get cookie and token
+    cookies = {
+        "CGISID": "NULL",
+    }
 
-	# Get cookie and token
-	cookies = {
-		'CGISID': 'NULL',
-	}
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101"
+            " Firefox/110.0"
+        ),
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "http://" + ip + "/login.html",
+        "Content-Type": "json",
+        "X-Requested-With": "XMLHttpRequest",
+        "Origin": "http://" + ip,
+        "Connection": "keep-alive",
+    }
 
-	headers = {
-		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0',
-		'Accept': 'application/json, text/javascript, */*; q=0.01',
-		'Accept-Language': 'en-US,en;q=0.5',
-		'Referer': 'http://'+ip+'/login.html',
-		'Content-Type': 'json',
-		'X-Requested-With': 'XMLHttpRequest',
-		'Origin': 'http://'+ip,
-		'Connection': 'keep-alive',
-	}
+    params = {
+        "_": "set_web_user_login_0.010597065160396713",
+    }
 
-	params = {
-		'_': 'set_web_user_login_0.010597065160396713',
-	}
+    data = '{"action":"set_web_user_login","args":{"user":"superadmin","password":"ccef6363409f6336f963053c63eb4363206363636363638363635a63633b6363206363636363638363635a63633b6363206363636363638363635a63633b6363"}}'
 
-	data = '{"action":"set_web_user_login","args":{"user":"superadmin","password":"ccef6363409f6336f963053c63eb4363206363636363638363635a63633b6363206363636363638363635a63633b6363206363636363638363635a63633b6363"}}'
+    response = requests.post(
+        "http://" + ip + "/cgi-bin/login.cgi",
+        params=params,
+        cookies=cookies,
+        headers=headers,
+        data=data,
+    )
 
-	response = requests.post('http://'+ip+'/cgi-bin/login.cgi', params=params, cookies=cookies, headers=headers, data=data)
+    aaa = response.headers["Set-Cookie"]
 
-	aaa=response.headers['Set-Cookie']
+    cookies = {"CGISID": aaa.split(";")[0].split("=")[1]}
 
-	cookies = {
-		'CGISID': aaa.split(';')[0].split('=')[1]
-	}
+    # Get data
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101"
+            " Firefox/110.0"
+        ),
+        "Accept": "application/json, text/javascript, */*; q=0.01",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Referer": "http://" + ip + "/index.html",
+        "Content-Type": "json",
+        "X-Requested-With": "XMLHttpRequest",
+        "Origin": "http://" + ip,
+        "Connection": "keep-alive",
+    }
 
-	# Get data
-	headers = {
-		'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0',
-		'Accept': 'application/json, text/javascript, */*; q=0.01',
-		'Accept-Language': 'en-US,en;q=0.5',
-		'Referer': 'http://'+ip+'/index.html',
-		'Content-Type': 'json',
-		'X-Requested-With': 'XMLHttpRequest',
-		'Origin': 'http://'+ip,
-		'Connection': 'keep-alive',
-	}
+    params = {
+        "_": "get_wwan_radio_info3_0.8110112846379409",
+    }
 
-	params = {
-		'_': 'get_wwan_radio_info3_0.8110112846379409',
-	}
+    data = (
+        '{"action":"get_wwan_radio_info3","token":"'
+        + response.json()["set_web_user_login"]["cgitoken"]
+        + '"}'
+    )
 
-	data = '{"action":"get_wwan_radio_info3","token":"'+response.json()['set_web_user_login']['cgitoken']+'"}'
+    while True:
+        response = requests.post(
+            "http://" + ip + "/cgi-bin/gui.cgi",
+            params=params,
+            cookies=cookies,
+            headers=headers,
+            data=data,
+        )
 
-	while True:
-		response = requests.post('http://'+ip+'/cgi-bin/gui.cgi', params=params, cookies=cookies, headers=headers, data=data)
+        resp = response.json()["get_wwan_radio_info3"]
+        rsrp = (
+            resp["nr_srv_cell_rsrp_0"],
+            resp["nr_srv_cell_rsrp_1"],
+            resp["nr_srv_cell_rsrp_2"],
+            resp["nr_srv_cell_rsrp_3"],
+        )
+        rsrq = resp["nr_srv_cell_rsrq"]
+        snr = resp["nr_snr"]
+        yield rsrp, rsrq, snr
 
-		resp = response.json()['get_wwan_radio_info3']
-		yield (resp['nr_srv_cell_rsrp_0'],resp['nr_srv_cell_rsrp_1'],resp['nr_srv_cell_rsrp_2'],resp['nr_srv_cell_rsrp_3'],)
 
-for i in get_data('192.168.1.1'):
-	print(i)
+for rsrp, rsrq, snr in get_data("192.168.1.1"):
+    print(f"RSRP: {rsrp}, RSRQ: {rsrq}, SNR: {snr}")

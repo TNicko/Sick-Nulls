61d5654d7cfb07f990debe0087be94e74cc223f2
czuhajster <55452766+czuhajster@users.noreply.github.com>
2023-10-25 23:51:08 +0200

    Fix main.py to make localization work.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pickle
 import numpy as np
-from sklearn.linear_model import LinearRegression
 import threading
 from time import sleep
 from mqtt_client import MQTTSubscriber
@@ -10,6 +9,7 @@ import requests
 from data_collector import initialize_coordinates_file
 from typing import Tuple
 from urllib3.exceptions import InsecureRequestWarning
+from sklearn.linear_model import LinearRegression
 
 
 def sick_beacon_coordinates() -> Tuple[float, float]:
@@ -17,7 +17,7 @@ def sick_beacon_coordinates() -> Tuple[float, float]:
         "accept": "application/json",
         "Authorization": (
             "Bearer"
-            " eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1MDczQjI0QUU0OUREOTUwNDJFQjg2RDZGMjdFOUYzIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2OTgyNjAyOTQsImV4cCI6MTY5ODI2MjA5NCwiaXNzIjoiaHR0cHM6Ly8xMjcuMC4wLjEvdXNlci1tYW5hZ2VyIiwiYXVkIjoiaHR0cDovL2Fzc2V0LWFuYWx5dGljcy5pby9hcGkiLCJjbGllbnRfaWQiOiJhc3NldC1hbmFseXRpY3MiLCJzdWIiOiI2NThlYmZmMi1hM2VkLTQ0Y2MtODkyYi0zYTZjZTVjY2Q1NjYiLCJhdXRoX3RpbWUiOjE2OTgyMjU3ODgsImlkcCI6ImxvY2FsIiwiaHR0cDovL2Fzc2V0LWFuYWx5dGljcy5pby9yb2xlIjoidmlld2VyIiwianRpIjoiOTYwNDVGMUM3MzQ5N0MwQUNCRjdCNkFCNDI3NkJCNDciLCJzaWQiOiJGMzc5OUU5QzAyQzRERDI3RTkxNDBFQ0M4NEExMkVDRCIsImlhdCI6MTY5ODI2MDI5NCwic2NvcGUiOlsiaHR0cDovL2Fzc2V0LWFuYWx5dGljcy5pby9hcGkiXSwiYW1yIjpbInB3ZCJdfQ.urTSGG25IIH9jO3A-d3lFw7eO29ycEOVY_1uU450qlhG0XJXleIGoN3MzrQJ0FIlBwN1IkLTjZvxs103mQH6GYAXr9qE9Jfe9iC9meQUm2HoccK9XwulbRgyVy2_SSTZ-y2wD7mdfwAljMl6_mb03V7epXx_g0e06vhsoEG9cIo3u3YlLDP0REnM2X0-SNQV5xrvIb6_KiEC9hmvBblsWGRaCns0-Id52Vr7-bmkDJw8NBZ4NsWenpg0cuBnkOWsKJOoI3l9d_424bjCoNhj5KXobjKy6wFhVn-lymnY5nmlSmXg9395OaQCBWiK5ujQ7zmgPDqAW0f0CCZ0qyHPeQ"
+            " eyJhbGciOiJSUzI1NiIsImtpZCI6IjI1MDczQjI0QUU0OUREOTUwNDJFQjg2RDZGMjdFOUYzIiwidHlwIjoiYXQrand0In0.eyJuYmYiOjE2OTgyNjg4MDksImV4cCI6MTY5ODI3MDYwOSwiaXNzIjoiaHR0cHM6Ly8xMjcuMC4wLjEvdXNlci1tYW5hZ2VyIiwiYXVkIjoiaHR0cDovL2Fzc2V0LWFuYWx5dGljcy5pby9hcGkiLCJjbGllbnRfaWQiOiJhc3NldC1hbmFseXRpY3MiLCJzdWIiOiI2NThlYmZmMi1hM2VkLTQ0Y2MtODkyYi0zYTZjZTVjY2Q1NjYiLCJhdXRoX3RpbWUiOjE2OTgyMjY1MTcsImlkcCI6ImxvY2FsIiwiaHR0cDovL2Fzc2V0LWFuYWx5dGljcy5pby9yb2xlIjoidmlld2VyIiwianRpIjoiM0Y5MTc1MDhCRjc4RDJFNDREQjM4NjE1QzNGNjBFRjIiLCJzaWQiOiI4Rjg4Rjg5NkQwNzg2QkIyQzY3RTQyRDk3NEQwQTk5OSIsImlhdCI6MTY5ODI2ODgwOSwic2NvcGUiOlsiaHR0cDovL2Fzc2V0LWFuYWx5dGljcy5pby9hcGkiXSwiYW1yIjpbInB3ZCJdfQ.GAY9yHvJnBGJFym51dYLfQLXd9xbqPqudoTouRdKu_Uq3_0dDy1v3iBJxv8q5Wa4fnxj7Zt0nZ9K6GshhWrsLvIQvItpev7nHuZU2nKax8NKCjVsyrzJl7ECXVY-d7Tggc_54LGUgB53So_LnvmIiALrrNhqE4F0BUQSXGwMrUIcog649EeWf9--pq3-AMiobEMMeay1N-WgvXxgDUGP-joOEnQ5JTC-m-J7I5UT5p4VpSU4wH3yARiF5V-rjMFuN5JSFJ7oDOLWBNGv_bsbJbY_3rfSz-eoyWalf5F8sB7pxwbXDtNDjeVxKDwds5ClSer7549tEjy3o2IS21H8bA"
         ),
     }
 
@@ -32,6 +32,7 @@ def sick_beacon_coordinates() -> Tuple[float, float]:
         headers=headers,
         verify=False,
     )
+    print(response.status_code)
     coordinates = response.json()["positions"][0]["position"]
     return (coordinates["x"], coordinates["y"])
 
@@ -71,8 +72,8 @@ def set_position(coords, theta):
         "llsX": coords[0],
         "llsY": coords[1],
         "llsTheta": theta,  # radians
-        "deviationRadius": 0.3,
-        "map": "SickHackathon_20231023.vmap",
+        "deviationRadius": 0.7,
+        "map": "Hackathon_20231025.vmap",
     }
 
     # Make the POST request
@@ -116,24 +117,46 @@ def main():
         agv_position = json_data["agvPosition"]
         x = agv_position["x"]
         y = agv_position["y"]
-        theta_rad = agv_position["theta"]
-        print(f"Initial theta: {theta_rad}")
         print(f"AGV coords: ", agv_position)
         theta_deg = math.degrees(theta_rad)
         agv_coords = (x, y)
 
-        for i in range(4):
-            set_position(beacon_coords, theta_rad)
+        theta_rad = 0.0
+        url = "http://172.16.0.242:32500/api/driveRotate"
+        headers = {
+            "accept": "application/json",
+            "authorization": "Basic cmFzcGk6c2VjcmV0UGFzc3dvcmQ=",
+            "Content-Type": "application/json",
+        }
+
+        angle_radians = 0.2* math.pi
+        data = {
+            "rotationDirection": "clockwise",
+            "angleRadians": angle_radians,
+            "speedRadianPerSecond": 0.02
+        }
+
+
+        theta_rad = angle_radians
+        for i in range(10):
+            set_position(transformed_beacon_coords[0], theta_rad)
+            sleep(5)
+            print(f"Beacon Coordinates: {transformed_beacon_coords}")
+            response = requests.post(url, headers=headers, json=data)
+            if response.status_code == 200:
+                print("Request was successful!")
+                #print("Response:", response.json())
+            else:
+                print(f"Request failed with status code {response.status_code}:")
+                print(response.text)
             matching_rate = get_matching_rate()
             print(f"Theta: {theta_rad} Matching rate: {matching_rate}")
             if matching_rate >= 90:
                 break
 
-            theta_rad += 0.5 * math.pi
-            if theta_rad > 2 * math.pi:
-                theta_rad -= 2 * math.pi
-
-            sleep(3)
+            theta_rad = angle_radians + 0.2 * math.pi
+            print(theta_rad)
+            sleep(5)
 
 
 if __name__ == "__main__":
